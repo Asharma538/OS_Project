fc45f97f9f528e8651b89b90fb85375088fa3d1b
Sukriti Goyal <goyal.23@iitj.ac.in>
2023-10-25 21:01:59 +0530

    Update update_details.py

--- app_monitor/update_details.py
+++ app_monitor/update_details.py
@@ -5,35 +5,35 @@ from datetime import datetime
 def get_pid_info( pid : int ):
     # os.system(f"ps -f -p {pid} > /home/enigmachine/Desktop/app_monitor/apps/{pid}/info.txt")
     apps = get_running_app_pids()
-    for i in apps:
+    for i in apps.keys():
         app_details = []
         new_start = -1
         new_pid = -1
         duration = 0
-        with open(f"Desktop/app_monitor/{i['app']}") as f:
+        with open(f"Desktop/app_monitor/{i}") as f:
             for l in f:
                 x = l.split(':')
                 app_details.append(x[1])
         if i['pid'] != app_details[2]:
             now = datetime.now()
             new_start = now.time()
-            new_pid = i['pid']
+            new_pid = apps[i]
             duration = new_start - app_details[3]
-            with open(f"app_monitor/{i['app']}") as f:
+            with open(f"app_monitor/{i}") as f:
                 l = f.readlines()
                 init = l[0].split(':')[1]
                 updated = int(init) + duration
                 replace = l[0][1].replace(l[0][1], str(updated))
                 l = replace
         
-        elif i['pid'] == '':
+        elif apps[i] == '':
                                     
             duration = new_start - app_details[3]
-            with open(f"app_monitor/{i['app']}") as f:
+            with open(f"app_monitor/{i}") as f:
                 l = f.readlines()
                 init = l[0].split(':')[1]
                 updated = int(init) + duration
                 replace = l[0][1].replace(l[0][1], str(updated))
                 l = replace
 
-        elif i['pid'] == app_details[2]:
+        elif apps[i] == app_details[2]:
