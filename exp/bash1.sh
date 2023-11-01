8f14d8668b3e117f6d63dd7410c5066963e5d165
Shubh Goyal <goyal.shubh0711@gmail.com>
2023-11-01 15:27:54 +0530

    code for window track
    tracker2.py track windows rather than

---
+++ exp/bash1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# zenity --info --text="Hello" --title="A Quote"
+current_date_time=$(date)
+echo "Kernel Version: $current_date_time" > /home/enigmachine/Documents/GitHub/OS_Project/hello.txt
